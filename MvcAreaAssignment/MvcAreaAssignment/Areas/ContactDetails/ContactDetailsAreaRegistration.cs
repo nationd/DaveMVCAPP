59ff472303c0fbff4a39a520d194191b79228991
David <davidm@bbd.co.za>
2014-06-10 11:30:23 +0200

    Nation MVC Area Assignment

---
+++ MvcAreaAssignment/MvcAreaAssignment/Areas/ContactDetails/ContactDetailsAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace MvcAreaAssignment.Areas.ContactDetails
+{
+    public class ContactDetailsAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ContactDetails";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ContactDetails_default",
+                "ContactDetails/{controller}/{action}/{id}",
+                defaults: new {controller = "Management", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "Areas.ContactDetails.Controllers" }
+            );
+        }
+    }
+}
+
